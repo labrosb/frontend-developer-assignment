e6985877ad3ec93126d58e47569c882ab15d3666
Lampros Barmpounis <labros_b@hotmail.com>
2024-08-11 23:06:57 +0200

    Chakra UI integrated

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,13 @@
+import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 import { ReactComponent as TimescaleLogo } from "../assets/logo.svg";
 
-const App = () => <TimescaleLogo />;
+const App: React.FC = () => {
+  return (
+    <ChakraProvider>
+      <TimescaleLogo />
+    </ChakraProvider>
+  );
+};
 
 export default App;
