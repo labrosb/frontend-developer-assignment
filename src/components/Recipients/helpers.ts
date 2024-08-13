d016ed9d8470a51acc7de60a74435c814a4ba3f7
Lampros Barmpounis <labros_b@hotmail.com>
2024-08-13 22:03:57 +0200

    Initial data format fixed

--- src/components/Recipients/helpers.ts
+++ src/components/Recipients/helpers.ts
@@ -1,25 +1,32 @@
-// Function groups recipients by domain (company) if more than one
-// and groups seperately the recipients that their email's domain is single
-export const groupRecipientsByDomain = (data: Recipient[]) => {
-  const domainGroups = data.reduce((acc: RecipientGroups, item: Recipient) => {
-    const domain = item.email.split("@")[1].split(".")[0];
-    if (!acc[domain]) {
-      acc[domain] = [];
-    }
-    acc[domain].push(item);
-    return acc;
-  }, {});
-
+// Function groups recipients by domain (company) if more than one in an array of arrays dataset
+// and groups seperately the recipients that their email's domain is single in a single array
+export const groupRecipientsByDomain = (recipients: Recipient[]) => {
   const singleDomains: Recipient[] = [];
-  const groupedDomains: RecipientGroups = {};
+  const domainGroups: [string, Recipient[]][] = [];
 
-  Object.keys(domainGroups).forEach((domain) => {
-    if (domainGroups[domain].length === 1) {
-      singleDomains.push(domainGroups[domain][0]);
-    } else {
-      groupedDomains[domain] = domainGroups[domain];
+  const domainMap = new Map<string, Recipient[]>();
+
+  recipients.forEach((recipient) => {
+    const domain = recipient.email.split("@")[1].split(".")[0];
+
+    let recipients = domainMap.get(domain);
+
+    if (!recipients) {
+      recipients = [];
+      domainMap.set(domain, recipients);
+    }
+
+    recipients.push(recipient);
+
+    if (recipients.length === 1) {
+      singleDomains.push(recipient);
+    } else if (recipients.length === 2) {
+      // Add the recipient to domainGroups and remove the first from singleDomains
+      domainGroups.push([domain, recipients]);
+      // Remove the last recipient added to singleDomains
+      singleDomains.splice(singleDomains.length - 1, 1);
     }
   });
 
-  return { singleDomains, groupedDomains };
+  return { singleDomains, domainGroups };
 };
